d5898839630b6b0b6f795ec7ae367e4d28cfe620
souhailsmaoui2004 <souhail.smaoui2017@gmail.com>
2024-07-10 20:59:45 +0100

    Commit new version

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const dotenv= require("dotenv")
 // declare une instance de pakage expresse
 const app=express()
 app.use(cors({
-    origin: 'http://localhost:3000' // Ou '*' pour permettre l'accès à partir de n'importe quel domaine
+    origin: '*'
+    // origin: 'http://localhost:3000' // Ou '*' pour permettre l'accès à partir de n'importe quel domaine
   }));
 const categorieRouter =require("./routes/categorie.route")
 const scategorieRouter =require("./routes/scategorie.route")
